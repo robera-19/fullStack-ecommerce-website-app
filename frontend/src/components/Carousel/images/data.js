5ecf866c27fe7caf2b11c2a54d9f45742a91592b
Robera Wakjira <robarobera50@gmail.com>
2026-04-16 13:47:52 +0300

    feat: install react-responsive-carousel

---
+++ frontend/src/components/Carousel/images/data.js
@@ -0,0 +1,6 @@
+import img1 from "../images/amazon-home-page.jpg"
+import img2 from "../images/amazon-home-page-2.jpg"
+import img3 from "../images/amazon-homepage-3.jpg"
+import img4 from "../images/amazon-homepage-4.jpg"
+
+export const img = [img1, img2, img3, img4];
