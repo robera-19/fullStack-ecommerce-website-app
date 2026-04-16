e176272804e3ce86851038f5420081494239e6cf
Robera Wakjira <robarobera50@gmail.com>
2026-04-16 15:35:36 +0300

    feat: add categories of things

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header";
 import CarouselEffect from "./components/Carousel/Carousel";
+import Category from "./components/Category/Category";
 
 const App = () => {
   return (
     <div>
       <Header />
       <CarouselEffect />
+      <Category />
     </div>
   );
 };
