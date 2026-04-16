63a5db28ceafb62fd39f2e1cd07394926b11e734
Robera Wakjira <robarobera50@gmail.com>
2026-04-16 15:32:39 +0300

    feat: add categories information

---
+++ frontend/src/components/Category/CategoriesData.js
@@ -0,0 +1,26 @@
+export const categories = [
+  {
+    title: "Electronics",
+    name: "electronics",
+    imgLink:
+      "https://i.pinimg.com/1200x/6d/dd/64/6ddd64c3de47d48cc81a06236c50e2d6.jpg",
+  },
+  {
+    title: "Fashion",
+    name: "fashion",
+    imgLink:
+      "https://i.pinimg.com/736x/10/3f/94/103f949e638c2c2c0dbd4db3b94ea25e.jpg",
+  },
+  {
+    title: "Home & Kitchen",
+    name: "home",
+    imgLink:
+      "https://i.pinimg.com/736x/45/c8/a3/45c8a3282e91e353b4e34a045813d764.jpg",
+  },
+  {
+    title: "Health & Beauty",
+    name: "beauty",
+    imgLink:
+      "https://i.pinimg.com/1200x/8c/9f/a7/8c9fa7dbc6e87d9a2d83c5bf0acf7874.jpg",
+  },
+];
