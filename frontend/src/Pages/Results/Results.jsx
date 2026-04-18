5bea1afcfd5adb15887ba29c4bde389d0eaf22dc
Robera Wakjira <robarobera50@gmail.com>
2026-04-18 11:06:34 +0300

    feat: add result page

---
+++ frontend/src/Pages/Results/Results.jsx
@@ -0,0 +1,48 @@
+import classes from "./Results.module.css";
+import Layout from "../../components/Layout/Layout";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { productURL } from "../../API/endPoints";
+import { useEffect, useState } from "react";
+import ProductCard from "../../components/Product/ProductCard";
+import Loader from "../../components/Loader/Loader";
+
+const Results = () => {
+  const [results, setResults] = useState([]);
+  const { categoryName } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    axios
+      .get(`${productURL}/products/category/${categoryName}`)
+      .then((res) => {
+        setResults(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+      });
+  }, []);
+
+  return (
+    <Layout>
+      <section>
+        <h1 style={{ padding: "30px" }}>Results</h1>
+        <p style={{ padding: "30px" }}>Category / {categoryName}</p>
+        <hr />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          <div className={classes.products_container}>
+            {results?.map((product) => {
+              return <ProductCard key={product.id} product={product} />;
+            })}
+          </div>
+        )}
+      </section>
+    </Layout>
+  );
+};
+
+export default Results;
