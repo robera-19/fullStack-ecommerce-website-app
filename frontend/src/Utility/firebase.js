80dac2441586f143f91f671548e7e5187edc3774
Robera Wakjira <robarobera50@gmail.com>
2026-04-22 10:43:44 +0300

    chore: setup firebase for authentication

---
+++ frontend/src/Utility/firebase.js
@@ -0,0 +1,22 @@
+// import { initializeApp } from "firebase/app";
+import firebase from "firebase/compat/app";
+
+import {getAuth} from 'firebase/auth'
+import "firebase/compat/firestore"
+import "firebase/compat/auth"
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9NP_CWLWjUpu-3KcnnS1YuQiK4PZLcVg",
+  authDomain: "clone-27e3b.firebaseapp.com",
+  projectId: "clone-27e3b",
+  storageBucket: "clone-27e3b.firebasestorage.app",
+  messagingSenderId: "597823912942",
+  appId: "1:597823912942:web:b6a4235467e0f2dfba2cff",
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export const db = app.firestore()
