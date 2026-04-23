9af7964e9c479a921a0f265e4123961962fea9ac
Robera Wakjira <robarobera50@gmail.com>
2026-04-23 12:35:44 +0300

    feat: add authentication functionality with firebase

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,30 @@
+import { useContext, useEffect } from "react";
 import Routing from "./Router.jsx";
+import { DataContext } from "./components/DataProvider/DataProvider.jsx";
+import { Type } from "./Utility/action.type.js";
+import { auth } from "./Utility/firebase.js";
 
 const App = () => {
+  const [{ user }, dispatch] = useContext(DataContext);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        // console.log(authUser);
+        dispatch({
+          type: Type.SET_USER,
+          user: authUser
+        })
+      }
+      else{
+        dispatch({
+          type: Type.SET_USER,
+          user: null,
+        });
+      }
+    });
+  }, []);
+
   return <Routing />;
 };
 
