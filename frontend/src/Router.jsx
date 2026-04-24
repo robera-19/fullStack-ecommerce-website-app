05ef66e32f33c47d7db468231a1c771fcfcb6c17
Robera Wakjira <robarobera50@gmail.com>
2026-04-24 21:07:23 +0300

    refactor: payment gateway path

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -5,7 +5,14 @@ import Payment from "./Pages/Payment/Payment";
 import Orders from "./Pages/Orders/Orders";
 import Cart from "./Pages/Cart/Cart";
 import Results from "./Pages/Results/Results";
-import ProductDetail from "./Pages/ProductDetail/ProductDetail"
+import ProductDetail from "./Pages/ProductDetail/ProductDetail";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+
+const stripePromise = loadStripe(
+  "pk_test_51TPRUIE73ZHNLij2l5muc5nHnZoLGqZgG5nsAGQF3Ac3hQ2o2OawmbkjKjwIXBeSVLKWQjnVwgPvm1D7MEyRTjBa00Hng4RI7a",
+);
 
 function Routing() {
   return (
@@ -13,7 +20,14 @@ function Routing() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/auth" element={<Auth />} />
-        <Route path="/payments" element={<Payment />} />
+        <Route
+          path="/payments"
+          element={
+            <Elements stripe={stripePromise}>
+              <Payment />
+            </Elements>
+          }
+        />
         <Route path="/orders" element={<Orders />} />
         <Route path="/category/:categoryName" element={<Results />} />
         <Route path="/products/:productId" element={<ProductDetail />} />
