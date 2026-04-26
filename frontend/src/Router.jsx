c479672805d2b0cb6d1d43a76ff680fbaf82f17e
Robera Wakjira <robarobera50@gmail.com>
2026-04-26 18:36:51 +0300

    feat: implement protected route on payment and orders page

--- frontend/src/Router.jsx
+++ frontend/src/Router.jsx
@@ -8,7 +8,7 @@ import Results from "./Pages/Results/Results";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-
+import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 
 const stripePromise = loadStripe(
   "pk_test_51TPRUIE73ZHNLij2l5muc5nHnZoLGqZgG5nsAGQF3Ac3hQ2o2OawmbkjKjwIXBeSVLKWQjnVwgPvm1D7MEyRTjBa00Hng4RI7a",
@@ -17,22 +17,37 @@ const stripePromise = loadStripe(
 function Routing() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route
-          path="/payments"
-          element={
-            <Elements stripe={stripePromise}>
-              <Payment />
-            </Elements>
-          }
-        />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/category/:categoryName" element={<Results />} />
-        <Route path="/products/:productId" element={<ProductDetail />} />
-        <Route path="/cart" element={<Cart />} />
-      </Routes>
+      <Elements stripe={stripePromise}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
+
+          <Route
+            path="/payments"
+            element={
+              <ProtectedRoute msg="you must log in to pay" redirect="/auth">
+                <Payment />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/orders"
+            element={
+              <ProtectedRoute
+                msg="you must log in to access your orders"
+                redirect="/auth"
+              >
+                <Orders />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="/category/:categoryName" element={<Results />} />
+          <Route path="/products/:productId" element={<ProductDetail />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Elements>
     </Router>
   );
 }
